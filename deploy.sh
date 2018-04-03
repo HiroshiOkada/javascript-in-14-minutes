207a76e01cb19a87d22ee11003e7317c58321c23
Hiroshi Okada <okadahiroshi@miobox.jp>
2018-04-03 19:02:06 +0900

    確実に deploy できるように deploy.sh を変更

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@
 
 git checkout gh-pages
 git merge master -m "Merge branch 'master' into gh-pages [ci skip]"
+npm run deploy
+git add .
+git commit -m "Build for deploy [ci skip]"
 git push origin gh-pages
